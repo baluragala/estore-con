15380186728e472f5956a329e75d5a6c7ff10c45
Bala Krishna Ragala <baluragala@gmail.com>
2018-03-07 17:17:59 +0530

    dynamic form

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -5,17 +5,20 @@ import { SharedModule } from "../shared/shared.module";
 import { ManageProductComponent } from "./manage-product/manage-product.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ManageProductReactiveComponent } from "./manage-product-reactive/manage-product-reactive.component";
+import { ReactiveOrderFormComponent } from "./reactive-order-form/reactive-order-form.component";
 @NgModule({
   imports: [CommonModule, SharedModule, FormsModule, ReactiveFormsModule],
   declarations: [
     ProductListItemComponent,
     ManageProductComponent,
-    ManageProductReactiveComponent
+    ManageProductReactiveComponent,
+    ReactiveOrderFormComponent
   ],
   exports: [
     ProductListItemComponent,
     ManageProductComponent,
-    ManageProductReactiveComponent
+    ManageProductReactiveComponent,
+    ReactiveOrderFormComponent
   ]
 })
 export class ProductModule {}
